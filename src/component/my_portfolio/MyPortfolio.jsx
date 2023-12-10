3f449ceba75708d12e70b5b653e1e27cb3ceefea
MohammadNasser-CS <abualnasser198@gmail.com>
2023-12-10 13:21:12 +0200

    final commit

--- src/component/my_portfolio/MyPortfolio.jsx
+++ src/component/my_portfolio/MyPortfolio.jsx
@@ -46,7 +46,7 @@ export default function MyPortfolio() {
                                         <i className={style.i_hover + " fab fa-github fs-4 text-white mx-4"} />
                                     </a>
                                     {/* <a href="#" className={style.icon}>
-                                        <i className={style.i_hover + " fab fa-behance fs-4 text-white mx-4"} />
+                                        <i className={style.i_hover + " fa-solid fa-globe fs-4 text-white mx-4"} />
                                     </a>
                                     <a href="#" className={style.icon}>
                                         <i className={style.i_hover + " fab fa-youtube fs-4 text-white mx-4"} />
@@ -59,93 +59,178 @@ export default function MyPortfolio() {
                                 <img src={require('./img/html1.jpg')} className={style.img_style} alt />
                             </div>
                             <div className={style.hover_items}>
-                                <h3 className="fs-4 mb-5 text-white fw-bolder">Project Source</h3>
+                            <h2 className="fs-2 mb-5 text-info-emphasis fw-bolder">Time Zone</h2>
+                                <h3 className="fs-4 mb-2 text-white fw-bolder">Project Source</h3>
                                 <div className="d-flex justify-content-center align-items-center">
-                                    <a href="#" className={style.icon}>
+                                    <a href="https://github.com/MohammadNasser-CS/timezone" target='_blank' className={style.icon}>
                                         <i className={style.i_hover + " fab fa-github fs-4 text-white mx-4"} />
                                     </a>
-                                    <a href="#" className={style.icon}>
-                                        <i className={style.i_hover + " fab fa-behance fs-4 text-white mx-4"} />
+                                    <a href="https://timezone-chi.vercel.app/" target='_blank' className={style.icon}>
+                                        <i className={style.i_hover + " fa-solid fa-globe fs-4 text-white mx-4"} />
                                     </a>
-                                    <a href="#" className={style.icon}>
+                                    {/* <a href="#" className={style.icon}>
                                         <i className={style.i_hover + " fab fa-youtube fs-4 text-white mx-4"} />
-                                    </a>
+                                    </a> */}
                                 </div>
                             </div>
                         </div>
                         <div className={style.port_item}>
                             <div className="image">
-                                <img src={require('./img/port4.jpg')} className={style.img_style} alt />
+                                <img src={require('./img/html1.jpg')} className={style.img_style} alt />
                             </div>
                             <div className={style.hover_items}>
-                                <h3 className="fs-4 mb-5 text-white fw-bolder">Project Source</h3>
+                            <h2 className="fs-2 mb-5 text-info-emphasis fw-bolder">Simple Restaurant</h2>
+                                <h3 className="fs-4 mb-2 text-white fw-bolder">Project Source</h3>
                                 <div className="d-flex justify-content-center align-items-center">
-                                    <a href="#" className={style.icon}>
+                                    <a href="https://github.com/MohammadNasser-CS/first_resturant" target="_blank" className={style.icon}>
                                         <i className={style.i_hover + " fab fa-github fs-4 text-white mx-4"} />
                                     </a>
-                                    <a href="#" className={style.icon}>
-                                        <i className={style.i_hover + " fab fa-behance fs-4 text-white mx-4"} />
+                                    <a href="https://first-resturant.vercel.app/" target="_blank" className={style.icon}>
+                                        <i className={style.i_hover + " fa-solid fa-globe fs-4 text-white mx-4"} />
                                     </a>
-                                    <a href="#" className={style.icon}>
+                                    {/* <a href="#" className={style.icon}>
                                         <i className={style.i_hover + " fab fa-youtube fs-4 text-white mx-4"} />
-                                    </a>
+                                    </a> */}
                                 </div>
                             </div>
                         </div>
                         <div className={style.port_item}>
                             <div className="image">
-                                <img src={require('./img/port5.jpg')} className={style.img_style} alt />
+                                <img src={require('./img/html1.jpg')} className={style.img_style} alt />
                             </div>
                             <div className={style.hover_items}>
-                                <h3 className="fs-4 mb-5 text-white fw-bolder">Project Source</h3>
+                            <h2 className="fs-2 mb-5 text-info-emphasis fw-bolder">CRUD</h2>
+                                <h3 className="fs-4 mb-2 text-white fw-bolder">Project Source</h3>
                                 <div className="d-flex justify-content-center align-items-center">
-                                    <a href="#" className={style.icon}>
+                                    <a href="https://github.com/MohammadNasser-CS/crud" target="_blank" className={style.icon}>
                                         <i className={style.i_hover + " fab fa-github fs-4 text-white mx-4"} />
                                     </a>
-                                    <a href="#" className={style.icon}>
-                                        <i className={style.i_hover + " fab fa-behance fs-4 text-white mx-4"} />
+                                    <a href="https://crud-six-ochre.vercel.app/" target="_blank" className={style.icon}>
+                                        <i className={style.i_hover + " fa-solid fa-globe fs-4 text-white mx-4"} />
                                     </a>
-                                    <a href="#" className={style.icon}>
+                                    {/* <a href="#" className={style.icon}>
                                         <i className={style.i_hover + " fab fa-youtube fs-4 text-white mx-4"} />
-                                    </a>
+                                    </a> */}
                                 </div>
                             </div>
                         </div>
                         <div className={style.port_item}>
                             <div className="image">
-                                <img src={require('./img/port6.jpg')} className={style.img_style} alt />
+                                <img src={require('./img/react4.png')} className={style.img_style} alt />
                             </div>
                             <div className={style.hover_items}>
-                                <h3 className="fs-4 mb-5 text-white fw-bolder">Project Source</h3>
+                            <h2 className="fs-2 mb-5 text-info-emphasis fw-bolder">React Training</h2>
+                                <h3 className="fs-4 mb-2 text-white fw-bolder">Project Source</h3>
                                 <div className="d-flex justify-content-center align-items-center">
-                                    <a href="#" className={style.icon}>
+                                    <a href="https://github.com/MohammadNasser-CS/startBootStrapExmaple" target="_blank" className={style.icon}>
                                         <i className={style.i_hover + " fab fa-github fs-4 text-white mx-4"} />
                                     </a>
-                                    <a href="#" className={style.icon}>
-                                        <i className={style.i_hover + " fab fa-behance fs-4 text-white mx-4"} />
+                                    <a href="https://start-boot-strap-exmaple.vercel.app/home" target="_blank" className={style.icon}>
+                                        <i className={style.i_hover + " fa-solid fa-globe fs-4 text-white mx-4"} />
                                     </a>
-                                    <a href="#" className={style.icon}>
+                                    {/* <a href="#" className={style.icon}>
                                         <i className={style.i_hover + " fab fa-youtube fs-4 text-white mx-4"} />
-                                    </a>
+                                    </a> */}
                                 </div>
                             </div>
                         </div>
                         <div className={style.port_item}>
                             <div className="image">
-                                <img src={require('./img/port7.jpg')} className={style.img_style} alt />
+                                <img src={require('./img/flutter1.png')} className={style.img_style} alt />
                             </div>
                             <div className={style.hover_items}>
-                                <h3 className="fs-4 mb-5 text-white fw-bolder">Project Source</h3>
+                            <h2 className="fs-2 mb-5 text-info-emphasis fw-bolder">News App</h2>
+                                <h3 className="fs-4 mb-2 text-white fw-bolder">Project Source</h3>
                                 <div className="d-flex justify-content-center align-items-center">
-                                    <a href="#" className={style.icon}>
+                                    <a href="https://github.com/MohammadNasser-CS/news-app-flutter" target="_blank" className={style.icon}>
                                         <i className={style.i_hover + " fab fa-github fs-4 text-white mx-4"} />
                                     </a>
-                                    <a href="#" className={style.icon}>
-                                        <i className={style.i_hover + " fab fa-behance fs-4 text-white mx-4"} />
+                                    {/* <a href="#" target="_blank" className={style.icon}>
+                                        <i className={style.i_hover + " fa-solid fa-globe fs-4 text-white mx-4"} />
+                                    </a> */}
+                                    {/* <a href="#" className={style.icon}>
+                                        <i className={style.i_hover + " fa-brands fa-dribbble fs-4 text-white mx-4"} />
+                                    </a> */}
+                                </div>
+                            </div>
+                        </div>
+                        <div className={style.port_item}>
+                            <div className="image">
+                                <img src={require('./img/flutter1.png')} className={style.img_style} alt />
+                            </div>
+                            <div className={style.hover_items}>
+                            <h2 className="fs-2 mb-5 text-info-emphasis fw-bolder">BMI Calculator App</h2>
+                                <h3 className="fs-4 mb-2 text-white fw-bolder">Project Source</h3>
+                                <div className="d-flex justify-content-center align-items-center">
+                                    <a href="https://github.com/MohammadNasser-CS/BMI-calculator" target="_blank" className={style.icon}>
+                                        <i className={style.i_hover + " fab fa-github fs-4 text-white mx-4"} />
                                     </a>
-                                    <a href="#" className={style.icon}>
-                                        <i className={style.i_hover + " fab fa-youtube fs-4 text-white mx-4"} />
+                                    {/* <a href="#" target="_blank" className={style.icon}>
+                                        <i className={style.i_hover + " fa-solid fa-globe fs-4 text-white mx-4"} />
+                                    </a> */}
+                                    {/* <a href="#" className={style.icon}>
+                                        <i className={style.i_hover + " fa-brands fa-dribbble fs-4 text-white mx-4"} />
+                                    </a> */}
+                                </div>
+                            </div>
+                        </div>
+                        <div className={style.port_item}>
+                            <div className="image">
+                                <img src={require('./img/flutter1.png')} className={style.img_style} alt />
+                            </div>
+                            <div className={style.hover_items}>
+                            <h2 className="fs-2 mb-5 text-info-emphasis fw-bolder">Food Delivery App</h2>
+                                <h3 className="fs-4 mb-2 text-white fw-bolder">Project Source</h3>
+                                <div className="d-flex justify-content-center align-items-center">
+                                    <a href="https://github.com/MohammadNasser-CS/food_delivery" target="_blank" className={style.icon}>
+                                        <i className={style.i_hover + " fab fa-github fs-4 text-white mx-4"} />
                                     </a>
+                                    {/* <a href="#" target="_blank" className={style.icon}>
+                                        <i className={style.i_hover + " fa-solid fa-globe fs-4 text-white mx-4"} />
+                                    </a> */}
+                                    {/* <a href="#" className={style.icon}>
+                                        <i className={style.i_hover + " fa-brands fa-dribbble fs-4 text-white mx-4"} />
+                                    </a> */}
+                                </div>
+                            </div>
+                        </div>
+                        <div className={style.port_item}>
+                            <div className="image">
+                                <img src={require('./img/flutter1.png')} className={style.img_style} alt />
+                            </div>
+                            <div className={style.hover_items}>
+                            <h2 className="fs-2 mb-5 text-info-emphasis fw-bolder">Todo-List App</h2>
+                                <h3 className="fs-4 mb-2 text-white fw-bolder">Project Source</h3>
+                                <div className="d-flex justify-content-center align-items-center">
+                                    <a href="https://github.com/MohammadNasser-CS/Todo_List_App" target="_blank" className={style.icon}>
+                                        <i className={style.i_hover + " fab fa-github fs-4 text-white mx-4"} />
+                                    </a>
+                                    {/* <a href="#" target="_blank" className={style.icon}>
+                                        <i className={style.i_hover + " fa-solid fa-globe fs-4 text-white mx-4"} />
+                                    </a> */}
+                                    {/* <a href="#" className={style.icon}>
+                                        <i className={style.i_hover + " fa-brands fa-dribbble fs-4 text-white mx-4"} />
+                                    </a> */}
+                                </div>
+                            </div>
+                        </div>
+                        <div className={style.port_item}>
+                            <div className="image">
+                                <img src={require('./img/flutter1.png')} className={style.img_style} alt />
+                            </div>
+                            <div className={style.hover_items}>
+                            <h2 className="fs-2 mb-5 text-info-emphasis fw-bolder">E-commerce App</h2>
+                                <h3 className="fs-4 mb-2 text-white fw-bolder">Project Source</h3>
+                                <div className="d-flex justify-content-center align-items-center">
+                                    <a href="https://github.com/MohammadNasser-CS/Ecommerce_app" target="_blank" className={style.icon}>
+                                        <i className={style.i_hover + " fab fa-github fs-4 text-white mx-4"} />
+                                    </a>
+                                    {/* <a href="#" target="_blank" className={style.icon}>
+                                        <i className={style.i_hover + " fa-solid fa-globe fs-4 text-white mx-4"} />
+                                    </a> */}
+                                    {/* <a href="#" className={style.icon}>
+                                        <i className={style.i_hover + " fa-brands fa-dribbble fs-4 text-white mx-4"} />
+                                    </a> */}
                                 </div>
                             </div>
                         </div>
